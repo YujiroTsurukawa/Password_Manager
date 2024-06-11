b55754d958390fa1707dff58f0ff379c8ebe385c
YujiroTsurukawa <ryu.yujiro0103@gmail.com>
2024-06-12 03:05:40 +0900

    パスワードマネージャー(試作)

---
+++ password_manager.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+info=("サービス名を入力してください:" "ユーザー名を入力してください:" "パスワードを入力してください:")
+
+echo "パスワードマネージャーへようこそ！"
+
+while true ; do
+  echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"; read input
+  case $input in
+  "Add Password" )
+     echo "サービス名を入力してください:"; read service_name
+     echo "ユーザー名を入力してください:"; read user_name
+     echo "パスワードを入力してください:"; read user_password
+     echo "パスワードの追加は成功しました。"
+     echo "$service_name $user_name $user_password" > "$service_name".txt
+  ;;  
+  "Get Password" )
+     echo "サービス名を入力してください:"; read service_name
+     array=($(cat $service_name.txt))
+     const=("サービス名:${array[0]}" "ユーザー名:${array[1]}" "パスワード:${array[2]}")
+
+     for i in ${const[@]}; do
+        echo "$i"
+     done
+  ;;
+  "Exit" )
+     echo "Thank you!"
+     break
+  ;;
+  * )
+     echo "入力が間違えています。Add Password/Get Password/Exit から入力してください:"
+  ;;
+  esac
+done
+#"パスワードの追加は成功しました。"
+#"次の選択肢から入力してください(Add Password/Get Password/Exit):"
+#"そのサービス名は登録されていません。"
+#"サービス名:"
+#"ユーザー名:"
+#"パスワード:"
+#"入力が間違えています。"
